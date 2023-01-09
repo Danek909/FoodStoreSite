e15080ea0ddbda97f2d1f7192d7bc5fc1d64dd41
Danya <kogdana@gmail.com>
2023-01-09 14:17:25 +0200

    new change from local repository

---
+++ js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    $('.divSlider').slick({
+        infinite: true,
+        slidesToScroll:1,
+        slidesToShow: 5,
+        arrows: true,
+        dots: false,
+        speed: 1000,
+        responsive: [
+
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1190,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+
+            {
+                breakpoint: 1166,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 425,
+                settings: {
+                    slidesToShow: 1.1,
+                }
+            },
+        ]
+    });
+});
+
+const factory = document.getElementById('burgerMenu');
+if (factory) {
+    const isHeder = document.getElementById('heder');
+    factory.addEventListener("click", function (e) {
+        isHeder.classList.toggle('_activee');
+    });
+}
+
+const divTop = document.getElementById('burgerMenu');
+if (factory) {
+    const isHeder = document.getElementById('divTop');
+    factory.addEventListener("click", function (e) {
+        isHeder.classList.toggle('_activee');
+    });
+}
+
+const body = document.getElementById('burgerMenu');
+if (factory) {
+    const isHeder = document.getElementById('body');
+    factory.addEventListener("click", function (e) {
+        isHeder.classList.toggle('_activee');
+    });
+}
